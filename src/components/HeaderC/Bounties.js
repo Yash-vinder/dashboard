36daa3b1ad5155f7df1efb10b4a8480c162c73e6
Yash-vinder <123yashvinder@gmail.com>
2022-12-14 18:10:59 +0530

    changes

--- src/components/HeaderC/Bounties.js
+++ src/components/HeaderC/Bounties.js
@@ -1,11 +1,165 @@
 import React from 'react'
+import {FaEdit} from 'react-icons/fa'
+import './Bounties.css'
+import profilePicture from '../images/icons-bounty.png';
 
 const Bounties = () => {
+  const myStyle = {
+    color:"white",
+    fontWeight:"600",
+  }
   return (
-    <>
-    <div className='conatiner'>
-      Bounties page
-    </div>
+
+    <>    <div className='main_bounty'>
+            <div className="user-detail">
+              <div className="user-icon">
+               <img src={profilePicture} alt="" />
+                  <div className='user-name'>
+                      <p>Courtney Henry</p>
+                        <span id='address'>@address</span>
+                   </div>
+                  </div>   
+                          <div className='user-prise'>
+                            <div>
+                            <div className="user-icon">
+                      <img src={profilePicture} alt="" />
+                          <div className='user-name'>
+                              <p>Courtney Henry</p>
+                                <span id='address'>@address</span>
+                          </div>
+                  </div> 
+                            </div>
+                            <div>price</div>
+                            <div><button className='btn_view_bounty'>View Bounty</button></div>
+                    </div>
+                  </div>
+                  <div className="user-detail">
+              <div className="user-icon">
+               <img src={profilePicture} alt="" />
+                  <div className='user-name'>
+                      <p>Courtney Henry</p>
+                        <span id='address'>@address</span>
+                         </div>
+                          </div>   
+                          <div className='user-prise'>
+                            <div>profile</div>
+                            <div>price</div>
+                            <div><button className='btn_view_bounty'>View Bounty</button></div>
+                    </div>
+                  </div>
+                  <div className="user-detail">
+              <div className="user-icon">
+               <img src={profilePicture} alt="" />
+                  <div className='user-name'>
+                      <p>Courtney Henry</p>
+                        <span id='address'>@address</span>
+                         </div>
+                          </div>   
+                          <div className='user-prise'>
+                            <div>profile</div>
+                            <div>price</div>
+                            <div><button className='btn_view_bounty'>View Bounty</button></div>
+                    </div>
+                  </div>
+                  <div className="user-detail">
+              <div className="user-icon">
+               <img src={profilePicture} alt="" />
+                  <div className='user-name'>
+                      <p>Courtney Henry</p>
+                        <span id='address'>@address</span>
+                         </div>
+                          </div>   
+                          <div className='user-prise'>
+                            <div>profile</div>
+                            <div>price</div>
+                            <div><button className='btn_view_bounty'>View Bounty</button></div>
+                    </div>
+                  </div>
+                  <div className="user-detail">
+              <div className="user-icon">
+               <img src={profilePicture} alt="" />
+                  <div className='user-name'>
+                      <p>Courtney Henry</p>
+                        <span id='address'>@address</span>
+                         </div>
+                          </div>   
+                          <div className='user-prise'>
+                            <div>profile</div>
+                            <div>price</div>
+                            <div><button className='btn_view_bounty'>View Bounty</button></div>
+                    </div>
+                  </div>
+                  <div className="user-detail">
+              <div className="user-icon">
+               <img src={profilePicture} alt="" />
+                  <div className='user-name'>
+                      <p>Courtney Henry</p>
+                        <span id='address'>@address</span>
+                         </div>
+                          </div>   
+                          <div className='user-prise'>
+                            <div>profile</div>
+                            <div>price</div>
+                            <div><button className='btn_view_bounty'>View Bounty</button></div>
+                    </div>
+                  </div>
+                  <div className="user-detail">
+              <div className="user-icon">
+               <img src={profilePicture} alt="" />
+                  <div className='user-name'>
+                      <p>Courtney Henry</p>
+                        <span id='address'>@address</span>
+                         </div>
+                          </div>   
+                          <div className='user-prise'>
+                            <div>profile</div>
+                            <div>price</div>
+                            <div><button className='btn_view_bounty'>View Bounty</button></div>
+                    </div>
+                  </div>
+                  <div className="user-detail">
+              <div className="user-icon">
+               <img src={profilePicture} alt="" />
+                  <div className='user-name'>
+                      <p>Courtney Henry</p>
+                        <span id='address'>@address</span>
+                         </div>
+                          </div>   
+                          <div className='user-prise'>
+                            <div>profile</div>
+                            <div>price</div>
+                            <div><button className='btn_view_bounty'>View Bounty</button></div>
+                    </div>
+                  </div>
+                  <div className="user-detail">
+              <div className="user-icon">
+               <img src={profilePicture} alt="" />
+                  <div className='user-name'>
+                      <p>Courtney Henry</p>
+                        <span id='address'>@address</span>
+                         </div>
+                          </div>   
+                          <div className='user-prise'>
+                            <div>profile</div>
+                            <div>price</div>
+                            <div><button className='btn_view_bounty'>View Bounty</button></div>
+                    </div>
+                  </div>
+                  <div className="user-detail">
+              <div className="user-icon">
+               <img src={profilePicture} alt="" />
+                  <div className='user-name'>
+                      <p>Courtney Henry</p>
+                        <span id='address'>@address</span>
+                         </div>
+                          </div>   
+                          <div className='user-prise'>
+                            <div>profile</div>
+                            <div>price</div>
+                            <div><button className='btn_view_bounty'>View Bounty</button></div>
+                    </div>
+                  </div>
+                  </div>
     </>
   )
 }
