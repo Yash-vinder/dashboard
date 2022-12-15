9a764c21b055daa719c721bcb1a32547729fd30d
Yash-vinder <123yashvinder@gmail.com>
2022-12-15 11:08:53 +0530

    this is the last changee

--- src/components/HeaderC/Approved.js
+++ src/components/HeaderC/Approved.js
@@ -3,8 +3,10 @@ import React from 'react'
 const Approved = () => {
   return (
     <>
-    <div>
-      Approved Funnction
+    <div className='container'>
+      <div className='row'>
+      <div>This is the card</div>
+      </div>
     </div>
     </>
   )
