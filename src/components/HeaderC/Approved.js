5ccb841b1a031412835b17edad2ac97f84610879
Yash-vinder <123yashvinder@gmail.com>
2022-12-15 12:33:33 +0530

    chnages in the Approved file

--- src/components/HeaderC/Approved.js
+++ src/components/HeaderC/Approved.js
@@ -1,11 +1,30 @@
 import React from 'react'
+import profilePicture from '../images/approved_profile.png';
+import './Approved.css'
 
 const Approved = () => {
   return (
     <>
-    <div className='container'>
+    <div className='container mt-5'>
       <div className='row'>
-      <div>This is the card</div>
+      <div className="user-detail">
+              <div className="user-icon">
+               <img src={profilePicture} alt="" />
+               <div className='user-name'>
+                      <p>Name: </p>
+                      <p>UserID: </p>
+                      <p>Bug Report: </p>
+                      <p>Remark: </p>
+                      </div>
+                  </div>   
+                          <div className='user-prise'>
+                            <div>
+                            
+                            </div>
+                            <div className='price_bounty'><p>$229</p></div>
+                            <div><button className='btn_view_bounty'>View Bounty</button></div>
+                    </div>
+                  </div>
       </div>
     </div>
     </>
